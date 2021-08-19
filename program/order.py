a81a9e1508a3633e11a38edcd36a592bb773e321
Maukan Mir <maukanm22@gmail.com>
2021-08-18 20:30:23 -0400

    almost done

--- program/order.py
+++ program/order.py
@@ -1,38 +1,35 @@
-
 import datetime
 
+
 class Order:
-    def __init__(self ):
-        self.orderID = None
-        self.orderItems = []
+    def __init__(self):
+        self.order_ID = None
+        self.order_items = []
         self.timestamp = datetime.datetime.now()
         self.tax = 0.095
-        self.orderTotal = 0
-        
+        self.order_Total = 0
+
     def getOrderID(self):
-        return self.orderID
+        return self.order_ID
 
     def getTimeStamp(self):
         return self.timestamp
 
-    def appendOrderItem(self,item) :
-        try:
-            self.orderItems.append(item)
-        except :
-            raise ValueError('Could not append item') 
+    def appendOrderItem(self, item):
+        return self.order_items.append(item)
 
-    def removeOrderItem(self, item ):
-        try :
-            pass
-        except :
-            raise ValueError('Could not remove item')
+    def removeOrderItem(self, item):
+
+        for index, i in enumerate(self.order_items):
+            if i == item:
+                self.order_items.remove(i)
 
     def calculateTotal(self):
-        for i in self.orderItems:
-            self.orderTotal += i.price #all objects have a price variable 
-        
-        return self.orderTotal* (1 + self.tax) #return sum of totals in order list * tax
-    
-            
-    def __repr__(self) :
-        return "{} {}".format(self.orderTotal , self.orderItems)
+        for i in self.order_Items:
+            self.order_Total += i.price  # all objects have a price variable
+
+        # return sum of totals in order list * tax
+        return self.order_Total * (1 + self.tax)
+
+    def __repr__(self):
+        return "{} {}".format(self.order_Total, self.order_Items)
