d74e02945e097d50233db97389a1ea37b1645ec2
Maukan Mir <maukanm22@gmail.com>
2021-08-18 21:07:10 -0400

    changes

--- program/order.py
+++ program/order.py
@@ -16,20 +16,47 @@ class Order:
         return self.timestamp
 
     def appendOrderItem(self, item):
-        return self.order_items.append(item)
+        self.order_items.append(item)
 
     def removeOrderItem(self, item):
-
         for index, i in enumerate(self.order_items):
             if i == item:
                 self.order_items.remove(i)
 
     def calculateTotal(self):
         for i in self.order_Items:
-            self.order_Total += i.price  # all objects have a price variable
+            try:
+                self.order_Total += i.price  # all objects have a price variable
+            except:
+                print("Item has no price variable")
 
         # return sum of totals in order list * tax
         return self.order_Total * (1 + self.tax)
 
     def __repr__(self):
         return "{} {}".format(self.order_Total, self.order_Items)
+
+
+#<---- Order Class Tests ------->
+test = Order()
+
+#<----- Order append Item Test ------->
+test.appendOrderItem('Pizza')
+test.appendOrderItem('Pasta')
+test.appendOrderItem('Salad')
+print(test.order_items)
+
+
+#<----- Remove Item Test ------->
+test.removeOrderItem('Pizza')
+test.removeOrderItem('Salad')
+test.removeOrderItem('Pizza')
+print(test.order_items)
+
+
+#<----- Datetime Test ------->
+print(test.getTimeStamp())
+
+
+
+
